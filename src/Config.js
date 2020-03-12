e376c9e37245c113457ac75b11186106c034dead
Philip Clement <pclement803@gmail.com>
2020-03-12 16:06:44 -0500

    making a new firebase

--- src/Config.js
+++ src/Config.js
@@ -1,11 +1,12 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyA-gur7D0AKt4MDfFKYz-RG7VYiCGbgmp0",
-  authDomain: "onehouse-b0a8d.firebaseapp.com",
-  databaseURL: "https://onehouse-b0a8d.firebaseio.com",
-  projectId: "onehouse-b0a8d",
-  storageBucket: "onehouse-b0a8d.appspot.com",
-  messagingSenderId: "1019945303315",
-  appID: "1:1019945303315:web:4a57b328bef39a09332ee6",
+  apiKey: "AIzaSyABgzeiiRyF-x5Ga_SjHI55emVvWV3UunY",
+  authDomain: "travis-testin.firebaseapp.com",
+  databaseURL: "https://travis-testin.firebaseio.com",
+  projectId: "travis-testin",
+  storageBucket: "travis-testin.appspot.com",
+  messagingSenderId: "298201200194",
+  appId: "1:298201200194:web:949c3bd3117d3c31851fdb",
+  measurementId: "G-7YTC7YJ8SM"
 };
 
 export default firebaseConfig;
