7ea302b417f19938c169384a0f19cfc1b0a433bd
Philip Clement <pclement803@gmail.com>
2020-03-12 14:37:23 -0500

    added dummy test

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,8 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+useless('useless test', ()=>{
+  expect(10+10).toEqual(20)
+  expect(true).toEqual(true)
+})
